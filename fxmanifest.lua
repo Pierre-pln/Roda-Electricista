db2399aca3286b584d5d1e1eb8950c93bf353caf
Pierre-pln <pierre.poulain.levrai@gmail.com>
2025-03-11 19:04:20 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+description 'Job electricien'
+author 'Pierre <3'
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+version '1.0.0'
+
+client_script {
+   'client.lua',
+}
+
+shared_scripts {
+    'Config.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+
+
